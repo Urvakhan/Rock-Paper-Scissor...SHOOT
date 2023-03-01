5d3619c2d17299992bf25472bcc06c12fbe99356
Paul Justice <pjustice5254@gmail.com>
2023-03-01 14:12:45 -0500

    changed code to follow actual instructions

--- brain.js
+++ brain.js
@@ -4,6 +4,10 @@
 //  - 3) player and computer values are compared
 //  - 4) winner is logged
 
+const playerChoice = prompt('Choose your Warrior');
+
+console.log(playerChoice);
+
 const options = ['rock', 'paper', 'scissors'];
 
 const optionsMap = {
@@ -30,51 +34,15 @@ const computerPick = () => {
     const random = Math.random();
     const length = options.length;
     const floor = Math.floor(random * length);
-    console.log(options[floor], random, length, floor)
     return options[floor]
 }
 
-// const determineWinner = (player, computer) => {
-//     if (player === 'rock') {
-//         if (computer === 'rock') {
-//             return 'Tie...'
-//         }
-//         if (computer === 'paper') {
-//             return 'Rock defeats Paper';
-//         }
-//         return 'Enemy Felled!';
-//     }
-
-//     if (player === 'paper') {
-//         if (computer === 'paper') {
-//             return 'Tie...'
-//         }
-//         if (computer === 'scissors') {
-//             return 'Scissors defeats Paper'
-//         }
-//         return 'Phyrric Victory!';
-//     }
-
-//     if (player === 'scissors') {
-//         if (computer === 'scissors'){
-//             return 'Tie...'
-//         }
-//         if (computer === 'rock') {
-//             return 'Rock defeats Scissors'
-//         }   
-//         return 'Foe Vanquished!';
-//     }
-    
-// }
-
-const play = (playerChoice) => {
+const play = () => {
     
     const computerChoice = computerPick();
 
-    // console.log(determineWinner(playerChoice, computerChoice))
+
     console.log(optionsMap[playerChoice][computerChoice]);
 }
 
-play('rock')
-play('paper')
-play('scissors')
+play()
